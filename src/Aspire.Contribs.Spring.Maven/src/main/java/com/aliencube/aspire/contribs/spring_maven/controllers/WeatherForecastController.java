c98390f02f16aba3a56f160a18c5291fa95e65e7
Justin Yoo <justin.yoojh@gmail.com>
2024-06-06 17:40:44 +0900

    Update Spring/Maven app

--- src/Aspire.Contribs.Spring.Maven/src/main/java/com/aliencube/aspire/contribs/spring_maven/controllers/WeatherForecastController.java
+++ src/Aspire.Contribs.Spring.Maven/src/main/java/com/aliencube/aspire/contribs/spring_maven/controllers/WeatherForecastController.java
@@ -1,14 +1,33 @@
 package com.aliencube.aspire.contribs.spring_maven.controllers;
 
+import java.time.LocalDate;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+import main.java.com.aliencube.aspire.contribs.spring_maven.models.WeatherForecast;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class WeatherForecastController {
 
-    @RequestMapping("/")
-    String home() {
-        return "Hello World!";
+    private final String[] summaries = {"Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"};
+
+    @GetMapping(value = "/weatherforecast", produces = MediaType.APPLICATION_JSON_VALUE)
+    public WeatherForecast[] getWeatherForecast() {
+        Random random = new Random();
+        return IntStream.range(1, 6)
+                .mapToObj(i -> new WeatherForecast(
+                        LocalDate.now().plusDays(i),
+                        random.nextInt(75) - 20,
+                        summaries[random.nextInt(summaries.length)]))
+                .toArray(WeatherForecast[]::new);
     }
 
 }
